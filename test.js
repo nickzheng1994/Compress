9541abfc14494aa54495750bc052b989391f531f
nick <853717893@qq.com>
2017-04-13 17:10:43 +0800

    no message

--- test.js
+++ test.js
@@ -2,10 +2,16 @@
 var UglifyJS = require("uglify-js"),
     fs = require("fs"),
     path = require("path"),
-    babel = require("babel-core");
+    babel = require("babel-core"),
+    colors = require('colors');
 
-let config = path.join(__dirname,'app.json'),
+let config = path.join(__dirname,'we_app','app.json'),
     data = JSON.parse(fs.readFileSync(config).toString());
+
+colors.setTheme({  
+    load: 'blue',  
+    finish: 'red',  
+});
 
 const compressCode = ()=>{
 
@@ -23,15 +29,18 @@ const compressCode = ()=>{
 
         let origCode = fs.readFileSync(config_path, 'utf8');
 
-        // let result = UglifyJS.minify(config_path);
         const opt = {
             presets: ['es2015']
         }
+        console.log(config_path + '正在压缩  '.load);
+
         let result = babel.transform(origCode,opt);
         
         result = UglifyJS.minify(result.code, {fromString: true});
 
         fs.writeFileSync(config_path, result.code);
+
+        console.log(config_path + '已压缩完毕  '.finish)
 
     });
 }
